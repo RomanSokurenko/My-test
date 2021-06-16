c158be70b222078e7193bc71154af0275b783e28
RomanSokurenko <sokurenko.roman82@gmail.com>
2021-06-16 09:34:57 +0300

    save

---
+++ src/js/Search-event.js
@@ -0,0 +1,44 @@
+// import debounce from 'lodash.debounce';
+// import getRefs from './get-Refs';
+// import eventTLP from '../tamplates/list.hbs';
+// import NewsApiService from './apiService';
+
+
+// const refs = getRefs();
+// const newsApiService = new NewsApiService();
+// refs.inputSearchForm.addEventListener('input', debounce(onInput, 2000));
+
+
+// if (newsApiService.query == 0) {
+//     randomList();
+// }
+
+// function onInput(e) {
+//   e.preventDefault();  
+//   newsApiService.query = e.target.value;  
+//   newsApiService.resetPage();
+//   clearContainer();
+//   fetchHits();  
+// };
+
+// function randomList() {
+//     newsApiService.fetchRandom().then(events => {
+//         appendMarkup(events);
+    
+//     });
+// }
+
+// function fetchHits() {    
+//     newsApiService.fetchArticles().then(events => {
+//         appendMarkup(events);                   
+//     });
+// }
+
+// function appendMarkup(events) { 
+//     refs.eventList.insertAdjacentHTML('beforeend', eventTLP(events));    
+// }
+  
+// function clearContainer() {
+//     refs.eventList.innerHTML = '';
+// }
+
---
+++ src/js/apiService.js
@@ -0,0 +1,43 @@
+// import { BASE_URL } from './baseData';
+// import { KEY } from './baseData';
+
+
+// export default class NewApiService{
+//     constructor() {        
+//         this.searchQuery = '';
+//         this.page = 1;
+//     }       
+    
+//     fetchArticles() {              
+//     return fetch(`${BASE_URL}events.json?keyword=${this.searchQuery}&apikey=${KEY}`)
+//             .then(r => r.json())
+//             .then(({ _embedded }) => {
+//                 // this.page += 1;
+//                 return  _embedded.events;
+//             })
+//             .catch(error => console.log(error));
+//     }
+
+//     fetchRandom() {       
+
+//         return fetch( `${BASE_URL}events.json?classificationName=music&sort=random&size=20&page=1&apikey=${KEY}`)
+//             .then(r => r.json())    
+//             .then(({ _embedded }) => {                
+//                 return  _embedded.events;
+//             })
+//             .catch(error => console.log(error));
+//     }
+    
+//     resetPage() {
+//         this.page = 1;
+//     }           
+    
+//     get  query() {
+//     return this.searchQuery;    
+//     }
+
+//     set  query(newQwery) {
+//      this.searchQuery=newQwery;    
+//     }
+// }
+
